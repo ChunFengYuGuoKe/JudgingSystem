37ea9c23e3f8b0db5a5ff8c77fafc365bdef2ef4
ChunFengYuGuoKe <1878223693@qq.com>
2024-04-03 10:06:07 +0800

    删除一些不必要的maven配置，添加文档

--- src/main/java/org/scu/judgingsystem/config/SwaggerStaticMappingConfiguration.java
+++ src/main/java/org/scu/judgingsystem/config/SwaggerStaticMappingConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -40,14 +39,11 @@ public class SwaggerStaticMappingConfiguration extends WebMvcConfigurationSuppor
 
     /**
      * 设置静态资源映射
-     * @param registry
+     * @param registry url映射注册器
      */
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("jfeiowhjfeuiowhgfuherug*********************************");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        // registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-        // super.addResourceHandlers(registry);
     }
 
 
